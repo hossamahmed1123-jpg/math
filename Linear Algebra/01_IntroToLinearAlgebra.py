c121e00a426d2ac78967dc00a2d8201109460127
hossamahmed1123-jpg <hossamahmed1123@gmail.com>
2026-07-20 16:31:19 +0300

    Getting Started to Linear Algebra
    Getting Started to Linear Algebra

---
+++ Linear Algebra/01_IntroToLinearAlgebra.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sys
+
+print(sys.version_info)
+
+asList = [1, 2, 3]
+asArray = np.array([1, 2, 3])  # 1D array
+rowVec = np.array([[1, 2, 3]])  # row
+colVec = np.array([[1], [2], [3]])  # column
+'''
+The Dimensions of Different Matrix 
+'''
+print(f'asList: {np.shape(asList)}')
+print(f'asArray: {asArray.shape}')
+print(f'rowVec: {rowVec.shape}')
+print(f'colVec: {colVec.shape}')
